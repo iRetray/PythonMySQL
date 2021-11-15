308d2d33992be73e3bd026902565d31885022766
Julian Camilo <julian20141016@gmail.com>
2021-11-15 17:06:19 -0500

    Created new remastered screens

--- remasteredScreens/actividades.py
+++ remasteredScreens/actividades.py
@@ -1,5 +1,4 @@
 from PyQt5 import QtCore, QtGui, QtWidgets
-
 import pymysql
 
 
@@ -13,7 +12,7 @@ def connect():
 
 class Ui_MainWindow(object):
 
-    def showName(self, none):
+    def addActivity(self, none):
         myConnection = connect()
         myCursor = myConnection.cursor()
         sql = "CALL newactividad("+self.inputID.text() + \
@@ -22,11 +21,48 @@ class Ui_MainWindow(object):
         print(sql)
         myCursor.execute(sql)
         myConnection.commit()
-        print("Finished SQL Query")
+        print("Finished SQL Procedure newactividad")
+
+    def searchByID(self, none):
+        myConnection = connect()
+        myCursor = myConnection.cursor()
+        sql = "CALL getactividadxid("+self.inputSearchID.text()+")"
+        print(sql)
+        myCursor.execute(sql)
+        objects = myCursor.fetchall()
+        print(objects)
+        myConnection.commit()
+        self.tableActivities.setRowCount(0)
+        for row_number, row_data in enumerate(objects):
+            self.tableActivities.insertRow(row_number)
+            for column_number, data in enumerate(row_data):
+                self.tableActivities.setItem(
+                    row_number, column_number, QtWidgets.QTableWidgetItem(str(data)))
+        print("Finished SQL Procedure getactividadxid")
+
+    def deleteByID(self, none):
+        myConnection = connect()
+        myCursor = myConnection.cursor()
+        sql = "CALL delactividades("+self.inputSearchID.text()+")"
+        print(sql)
+        myCursor.execute(sql)
+        myConnection.commit()
+        print("Finished SQL Procedure delactividades")
+
+    def modifyActivity(self, none):
+        myConnection = connect()
+        myCursor = myConnection.cursor()
+        sql = "CALL modactividades("+self.inputID.text() + \
+            ",'"+self.inputName.text()+"','"+self.inputDescription.text() + \
+            "','"+self.inputDifficulty.text()+"',NULL,"+self.inputSearchID.text()+")"
+        print(sql)
+        myCursor.execute(sql)
+        myConnection.commit()
+        print("Finished SQL Procedure modactividades")
 
     def setupUi(self, MainWindow):
         MainWindow.setObjectName("MainWindow")
-        MainWindow.resize(800, 600)
+        MainWindow.resize(702, 554)
         self.centralwidget = QtWidgets.QWidget(MainWindow)
         self.centralwidget.setObjectName("centralwidget")
         self.widget = QtWidgets.QWidget(self.centralwidget)
@@ -82,24 +118,24 @@ class Ui_MainWindow(object):
         self.label_30 = QtWidgets.QLabel(self.widget_2)
         self.label_30.setGeometry(QtCore.QRect(20, 280, 20, 20))
         self.label_30.setObjectName("label_30")
-        self.inputID2 = QtWidgets.QLineEdit(self.widget_2)
-        self.inputID2.setGeometry(QtCore.QRect(50, 280, 113, 20))
-        self.inputID2.setObjectName("inputID2")
-        self.BbuscarActividades = QtWidgets.QPushButton(self.widget_2)
-        self.BbuscarActividades.setGeometry(QtCore.QRect(210, 280, 81, 21))
-        self.BbuscarActividades.setStyleSheet("background-color: rgb(255, 0, 127);\n"
-                                              "background-color: rgb(255, 255, 0);")
-        self.BbuscarActividades.setObjectName("BbuscarActividades")
-        self.BeliminarActividades = QtWidgets.QPushButton(self.widget_2)
-        self.BeliminarActividades.setGeometry(QtCore.QRect(320, 280, 75, 23))
-        self.BeliminarActividades.setStyleSheet("background-color: rgb(255, 0, 127);\n"
-                                                "background-color: rgb(255, 255, 0);")
-        self.BeliminarActividades.setObjectName("BeliminarActividades")
-        self.BmodificarActividades = QtWidgets.QPushButton(self.widget_2)
-        self.BmodificarActividades.setGeometry(QtCore.QRect(420, 280, 75, 23))
-        self.BmodificarActividades.setStyleSheet("background-color: rgb(255, 0, 127);\n"
-                                                 "background-color: rgb(255, 255, 0);")
-        self.BmodificarActividades.setObjectName("BmodificarActividades")
+        self.inputSearchID = QtWidgets.QLineEdit(self.widget_2)
+        self.inputSearchID.setGeometry(QtCore.QRect(50, 280, 113, 20))
+        self.inputSearchID.setObjectName("inputSearchID")
+        self.buttonSearch = QtWidgets.QPushButton(self.widget_2)
+        self.buttonSearch.setGeometry(QtCore.QRect(210, 280, 81, 21))
+        self.buttonSearch.setStyleSheet("background-color: rgb(255, 0, 127);\n"
+                                        "background-color: rgb(255, 255, 0);")
+        self.buttonSearch.setObjectName("buttonSearch")
+        self.buttonDelete = QtWidgets.QPushButton(self.widget_2)
+        self.buttonDelete.setGeometry(QtCore.QRect(320, 280, 75, 23))
+        self.buttonDelete.setStyleSheet("background-color: rgb(255, 0, 127);\n"
+                                        "background-color: rgb(255, 255, 0);")
+        self.buttonDelete.setObjectName("buttonDelete")
+        self.buttonModify = QtWidgets.QPushButton(self.widget_2)
+        self.buttonModify.setGeometry(QtCore.QRect(420, 280, 75, 23))
+        self.buttonModify.setStyleSheet("background-color: rgb(255, 0, 127);\n"
+                                        "background-color: rgb(255, 255, 0);")
+        self.buttonModify.setObjectName("buttonModify")
         self.label = QtWidgets.QLabel(self.widget_2)
         self.label.setGeometry(QtCore.QRect(50, 120, 21, 16))
         self.label.setObjectName("label")
@@ -116,12 +152,10 @@ class Ui_MainWindow(object):
         self.label_3.setGeometry(QtCore.QRect(380, 120, 71, 16))
         self.label_3.setObjectName("label_3")
         self.inputDescription = QtWidgets.QLineEdit(self.widget_2)
-        self.inputDescription.setGeometry(
-            QtCore.QRect(380, 140, 141, 31))
+        self.inputDescription.setGeometry(QtCore.QRect(380, 140, 141, 31))
         self.inputDescription.setObjectName("inputDescription")
         self.inputDifficulty = QtWidgets.QLineEdit(self.widget_2)
-        self.inputDifficulty.setGeometry(
-            QtCore.QRect(50, 210, 151, 31))
+        self.inputDifficulty.setGeometry(QtCore.QRect(50, 210, 151, 31))
         self.inputDifficulty.setObjectName("inputDifficulty")
         self.label_4 = QtWidgets.QLabel(self.widget_2)
         self.label_4.setGeometry(QtCore.QRect(50, 190, 71, 16))
@@ -130,27 +164,35 @@ class Ui_MainWindow(object):
         self.label_5.setGeometry(QtCore.QRect(220, 190, 81, 16))
         self.label_5.setObjectName("label_5")
         self.inputCoordinator = QtWidgets.QLineEdit(self.widget_2)
-        self.inputCoordinator.setGeometry(
-            QtCore.QRect(220, 210, 141, 31))
+        self.inputCoordinator.setGeometry(QtCore.QRect(220, 210, 141, 31))
         self.inputCoordinator.setObjectName("inputCoordinator")
         self.buttonAddActivity = QtWidgets.QPushButton(self.widget_2)
         self.buttonAddActivity.setGeometry(QtCore.QRect(560, 250, 75, 23))
         self.buttonAddActivity.setStyleSheet(
             "background-color: rgb(255, 255, 0);")
         self.buttonAddActivity.setObjectName("buttonAddActivity")
-        self.T_Actividades = QtWidgets.QTreeWidget(self.widget_2)
-        self.T_Actividades.setGeometry(QtCore.QRect(20, 310, 621, 151))
-        self.T_Actividades.setObjectName("T_Actividades")
+        self.tableActivities = QtWidgets.QTableWidget(self.widget_2)
+        self.tableActivities.setEnabled(True)
+        self.tableActivities.setGeometry(QtCore.QRect(20, 310, 621, 151))
+        self.tableActivities.setObjectName("tableActivities")
+        self.tableActivities.setColumnCount(5)
+        self.tableActivities.setRowCount(0)
+        item = QtWidgets.QTableWidgetItem()
+        self.tableActivities.setHorizontalHeaderItem(0, item)
+        item = QtWidgets.QTableWidgetItem()
+        self.tableActivities.setHorizontalHeaderItem(1, item)
+        item = QtWidgets.QTableWidgetItem()
+        self.tableActivities.setHorizontalHeaderItem(2, item)
+        item = QtWidgets.QTableWidgetItem()
+        self.tableActivities.setHorizontalHeaderItem(3, item)
+        item = QtWidgets.QTableWidgetItem()
+        self.tableActivities.setHorizontalHeaderItem(4, item)
         self.BSalirActividades = QtWidgets.QPushButton(self.widget_2)
         self.BSalirActividades.setGeometry(QtCore.QRect(40, 50, 75, 23))
         self.BSalirActividades.setStyleSheet(
             "background-color: rgb(255, 0, 0);")
         self.BSalirActividades.setObjectName("BSalirActividades")
         MainWindow.setCentralWidget(self.centralwidget)
-        self.menubar = QtWidgets.QMenuBar(MainWindow)
-        self.menubar.setGeometry(QtCore.QRect(0, 0, 800, 21))
-        self.menubar.setObjectName("menubar")
-        MainWindow.setMenuBar(self.menubar)
         self.statusbar = QtWidgets.QStatusBar(MainWindow)
         self.statusbar.setObjectName("statusbar")
         MainWindow.setStatusBar(self.statusbar)
@@ -173,25 +215,29 @@ class Ui_MainWindow(object):
         self.label_29.setText(_translate(
             "MainWindow", "INGRESE EL ID PARA BUSCAR,ELIMINAR O MODIFICAR"))
         self.label_30.setText(_translate("MainWindow", "ID:"))
-        self.BbuscarActividades.setText(_translate("MainWindow", "BUSCAR"))
-        self.BeliminarActividades.setText(_translate("MainWindow", "ELIMINAR"))
-        self.BmodificarActividades.setText(
-            _translate("MainWindow", "MODIFICAR"))
+        self.buttonSearch.setText(_translate("MainWindow", "BUSCAR"))
+        self.buttonSearch.clicked.connect(self.searchByID)
+        self.buttonDelete.setText(_translate("MainWindow", "ELIMINAR"))
+        self.buttonDelete.clicked.connect(self.deleteByID)
+        self.buttonModify.setText(_translate("MainWindow", "MODIFICAR"))
+        self.buttonModify.clicked.connect(self.modifyActivity)
         self.label.setText(_translate("MainWindow", "ID :"))
         self.label_2.setText(_translate("MainWindow", "NOMBRE:"))
         self.label_3.setText(_translate("MainWindow", "DESCRIPCION:"))
         self.label_4.setText(_translate("MainWindow", "DIFICULTAD:"))
         self.label_5.setText(_translate("MainWindow", "COORDINADOR:"))
         self.buttonAddActivity.setText(_translate("MainWindow", "AGREGAR"))
-        self.buttonAddActivity.clicked.connect(self.showName)
-        self.T_Actividades.headerItem().setText(0, _translate("MainWindow", "ID:"))
-        self.T_Actividades.headerItem().setText(1, _translate("MainWindow", "NOMBRE:"))
-        self.T_Actividades.headerItem().setText(
-            2, _translate("MainWindow", "DESCRIPCION:"))
-        self.T_Actividades.headerItem().setText(
-            3, _translate("MainWindow", "DIFICULTAD:"))
-        self.T_Actividades.headerItem().setText(
-            4, _translate("MainWindow", "COORDINADOR:"))
+        self.buttonAddActivity.clicked.connect(self.addActivity)
+        item = self.tableActivities.horizontalHeaderItem(0)
+        item.setText(_translate("MainWindow", "ID"))
+        item = self.tableActivities.horizontalHeaderItem(1)
+        item.setText(_translate("MainWindow", "Nombre"))
+        item = self.tableActivities.horizontalHeaderItem(2)
+        item.setText(_translate("MainWindow", "Descripcion"))
+        item = self.tableActivities.horizontalHeaderItem(3)
+        item.setText(_translate("MainWindow", "Dificultad"))
+        item = self.tableActivities.horizontalHeaderItem(4)
+        item.setText(_translate("MainWindow", "Coordinador"))
         self.BSalirActividades.setText(_translate("MainWindow", "SALIR"))
 
 
